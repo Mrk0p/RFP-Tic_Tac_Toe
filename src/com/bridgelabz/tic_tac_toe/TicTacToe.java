cf0a22c72ed068fc2cc957ef39d9ffeae28f1742
Mrk0p <sachinwele23@gmail.com>
2023-01-19 16:03:40 +0530

    Added : display TicTacToe board

--- src/com/bridgelabz/tic_tac_toe/TicTacToe.java
+++ src/com/bridgelabz/tic_tac_toe/TicTacToe.java
@@ -21,10 +21,19 @@ public class TicTacToe {
         userLetter = scanner.next().toUpperCase().charAt(0);
         computerLetter = (userLetter == 'X') ? 'O' : 'X';
     }
+    private static void showBoard()
+    {
+        System.out.println( board[1] + " | " + board[2] + " | " + board[3] );
+        System.out.println("----------");
+        System.out.println( board[4] + " | " + board[5] + " | " + board[6] );
+        System.out.println("----------");
+        System.out.println( board[7] + " | " + board[8] + " | " + board[9] );
+    }
     public static void main(String[] args)
     {
         createEmptyBoard();
         chooseLetter();
+        showBoard();
 
     }
 
