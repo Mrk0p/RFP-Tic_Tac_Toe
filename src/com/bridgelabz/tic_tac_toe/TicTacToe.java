5cb5105aad85d32e97d08ceef77dc52186929cad
Sachin wele <115727568+Mrk0p@users.noreply.github.com>
2023-01-20 10:55:29 +0530

    Added : Choose a Letter between X or O

--- src/com/bridgelabz/tic_tac_toe/TicTacToe.java
+++ src/com/bridgelabz/tic_tac_toe/TicTacToe.java
@@ -2,11 +2,15 @@ package com.bridgelabz.tic_tac_toe;
 
 import java.util.Scanner;
 public class TicTacToe {
-
+    
+    // create an array
     static char[] board = new char[10];
     static char userLetter;
     static char computerLetter;
-
+    
+    // create a method for iteration 
+    // for loop goes 1 to 9
+         
     private static void createEmptyBoard()
     {
         for(int index=1; index<board.length;index++)
@@ -14,6 +18,9 @@ public class TicTacToe {
             board[index] = ' ';
         }
     }
+    
+    // method for choose X or O
+    // using ternary operator ? exp1:exp2
     private static void chooseLetter()
     {
         Scanner scanner = new Scanner(System.in);
